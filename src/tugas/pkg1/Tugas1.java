068b0c13dd4d83e0745806ea84c4d60003d2cb0e
AdityaPaguno <ap2056609@gmail.com>
2018-09-07 11:47:20 +0700

    Test 1

---
+++ src/tugas/pkg1/Tugas1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.pkg1;
+
+/**
+ *
+ */
+public class Tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int jumlah = 5;
+        for(int k=0;k<jumlah;k++){
+            for(int l=0;l<=k;l++){
+                System.out.print("*"); 
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
